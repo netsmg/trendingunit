a51d213cae2964441cefde91609a90599ffed01a
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-01-31 00:28:44 +0600

    Update Header.svelte

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,7 +3,136 @@
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
 </script>
+<!-- header -->
+	<header class="header">
+		<div class="header__wrap">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="header__content">
+							<button class="header__menu" type="button">
+								<span></span>
+								<span></span>
+								<span></span>
+							</button>
 
+							<a href="/" class="header__logo">
+								<img src="https://trendingunit.vercel.app/logo.svg"/>
+							</a>
+
+							<ul class="header__nav">
+								<li class="header__nav-item">
+									<a class="header__nav-link" href="/">Home</a>
+								</li>
+								<li class="header__nav-item">
+									<a class="header__nav-link" href="#" role="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Catalog</a>
+
+									<ul class="dropdown-menu header__nav-menu" aria-labelledby="dropdownMenu1">
+										<li><a href="/">Catalog</a></li>
+										<li><a href="/">Details</a></li>
+									</ul>
+								</li>
+								<li class="header__nav-item">
+									<a class="header__nav-link" href="#" role="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">News</a>
+
+									<ul class="dropdown-menu header__nav-menu" aria-labelledby="dropdownMenu2">
+										<li><a href="/">News</a></li>
+										<li><a href="/">Article</a></li>
+										<li><a href="/">Interview</a></li>
+									</ul>
+								</li>
+								<li class="header__nav-item">
+									<a class="header__nav-link" href="/">Help Center</a>
+								</li>
+								<li class="header__nav-item">
+									<a class="header__nav-link" href="#" role="button" id="dropdownMenu3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<svg xmlns='http://www.w3.org/2000/svg' width='512' height='512' viewBox='0 0 512 512'><circle cx='256' cy='256' r='32' style='fill:none; stroke-miterlimit:10;stroke-width:32px'/><circle cx='416' cy='256' r='32' style='fill:none;stroke-miterlimit:10;stroke-width:32px'/><circle cx='96' cy='256' r='32' style='fill:none;stroke-miterlimit:10;stroke-width:32px'/></svg>
+									</a>
+
+									<ul class="dropdown-menu header__nav-menu header__nav-menu--scroll" aria-labelledby="dropdownMenu3">
+										<li><a href="/">Checkout</a></li>
+										<li><a href="/">Favorites</a></li>
+										<li><a href="./about">About</a></li>
+										<li><a href="/">Profile</a></li>
+										<li><a href="./login">Sign in</a></li>
+										<li><a href="./register">Sign up</a></li>
+										<li><a href="/">Forgot password</a></li>
+										<li><a href="./privacy">Privacy policy</a></li>
+										<li><a href="/">Contacts</a></li>
+										<li><a href="/">404 Page</a></li>
+									</ul>
+								</li>
+							</ul>
+
+							<div class="header__actions">
+								<div class="header__lang">
+									<a class="header__lang-btn" href="#" role="button" id="dropdownMenuLang" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<img src="https://flixhub.volkovdesign.com/img/flags/uk.svg" alt="">
+										<span>EN</span>
+										<svg xmlns='http://www.w3.org/2000/svg' width='512' height='512' viewBox='0 0 512 512'><path d='M98,190.06,237.78,353.18a24,24,0,0,0,36.44,0L414,190.06c13.34-15.57,2.28-39.62-18.22-39.62H116.18C95.68,150.44,84.62,174.49,98,190.06Z'/></svg>
+									</a>
+
+									<ul class="dropdown-menu header__lang-menu" aria-labelledby="dropdownMenuLang">
+										<li><a href="/"><img src="https://flixhub.volkovdesign.com/img/flags/spain.svg" alt=""><span>SP</span></a></li>
+										<li><a href="#"><img src="https://flixhub.volkovdesign.com/img/flags/russia.svg" alt=""><span>RU</span></a></li>
+										<li><a href="#"><img src="https://flixhub.volkovdesign.com/img/flags/china.svg" alt=""><span>CH</span></a></li>
+									</ul>
+								</div>
+
+								<a href="./login" class="header__login">
+									<svg xmlns='http://www.w3.org/2000/svg' width='512' height='512' viewBox='0 0 512 512'><path d='M192,176V136a40,40,0,0,1,40-40H392a40,40,0,0,1,40,40V376a40,40,0,0,1-40,40H240c-22.09,0-48-17.91-48-40V336' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/><polyline points='288 336 368 256 288 176' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/><line x1='80' y1='256' x2='352' y2='256' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/></svg>
+									<span>Sign in</span>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="header__wrap">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="header__content">
+							<form action="#" class="header__form">
+								<input type="text" class="header__input" placeholder="I'm searching for...">
+								<select class="header__select">
+									<option value="0">All Categories</option>
+									<option value="1">Action</option>
+									<option value="3">Adventure</option>
+									<option value="4">Fighting</option>
+									<option value="5">Flight simulation</option>
+									<option value="6">Platform</option>
+									<option value="7">Racing</option>
+									<option value="8">RPG</option>
+									<option value="9">Sports</option>
+									<option value="10">Strategy</option>
+									<option value="11">Survival horror</option>
+								</select>
+								<button class="header__btn" type="button">
+									<svg xmlns='http://www.w3.org/2000/svg' width='512' height='512' viewBox='0 0 512 512'><path d='M221.09,64A157.09,157.09,0,1,0,378.18,221.09,157.1,157.1,0,0,0,221.09,64Z' style='fill:none;stroke-miterlimit:10;stroke-width:32px'/><line x1='338.29' y1='338.29' x2='448' y2='448' style='fill:none;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px'/></svg>
+								</button>
+							</form>
+
+							<div class="header__actions header__actions--2">
+								<a href="/" class="header__link">
+									<svg xmlns='http://www.w3.org/2000/svg' width='512' height='512' viewBox='0 0 512 512'><path d='M352.92,80C288,80,256,144,256,144s-32-64-96.92-64C106.32,80,64.54,124.14,64,176.81c-1.1,109.33,86.73,187.08,183,252.42a16,16,0,0,0,18,0c96.26-65.34,184.09-143.09,183-252.42C447.46,124.14,405.68,80,352.92,80Z' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/></svg>
+									<span>Favorites</span>
+								</a>
+
+								<a href="./" class="header__link">
+									<svg xmlns='http://www.w3.org/2000/svg' width='512' height='512' viewBox='0 0 512 512'><circle cx='176' cy='416' r='16' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/><circle cx='400' cy='416' r='16' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/><polyline points='48 80 112 80 160 352 416 352' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/><path d='M160,288H409.44a8,8,0,0,0,7.85-6.43l28.8-144a8,8,0,0,0-7.85-9.57H128' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/></svg>
+									<span>$00.00</span>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	
 <header>
 	<div class="corner">
 <a href="/">
@@ -40,92 +169,3 @@
 	</div>
 </header>
 
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--color-theme-1);
-	}
-</style>
