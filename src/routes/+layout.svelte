5d34f9f6a0b9b58714574575f7586f8a1720117c
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-01-31 10:21:17 +0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
 	import { webVitals } from '$lib/vitals';
-	import Header from './Header.svelte';
+	
 	import './styles.css';
 
 	/** @type {import('./$types').LayoutServerData} */
@@ -18,7 +18,7 @@
 </script>
 
 <div class="app">
-	<Header />
+	
 
 	<main>
 		<slot />
