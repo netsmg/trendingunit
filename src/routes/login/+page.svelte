81cf58d67527412e1728447510cca837baadc9d8
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-02-03 23:05:46 +0600

    Update +page.svelte

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import { onMount } from 'svelte';
-  import { fauth, db } from "$firebase";
+  import { fauth, db } from "../firebase.js";
   
   import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import {goto} from '$app/navigation';
