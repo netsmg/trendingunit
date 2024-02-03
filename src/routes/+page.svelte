c3728d5cd32c022327d45ee48da90c34e777ce13
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-02-03 23:53:05 +0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,7 +30,24 @@
 
   export let year = new Date().getFullYear();
 </script>
-
+<head>
+    <meta charset="utf-8" />
+    <title>Log in | Webui</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta content="Responsive Bootstrap 5 Chat App" name="description" />
+    <meta content="Netsmg" name="author" />
+    <!-- App favicon -->
+    
+    <link
+      href="https://cdn.jsdelivr.net/npm/remixicon@2.3.0/fonts/remixicon.css"
+      rel="stylesheet"
+    />
+    <link href="./css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="./css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="./css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+  </head>
 <div class="account-pages my-5 pt-sm-5">
   <div class="container">
     <div class="row justify-content-center">
