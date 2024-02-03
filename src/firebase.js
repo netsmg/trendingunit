34bd8e816f748056b734714d9ad28dce0f065794
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-02-03 18:47:25 +0600

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// firebase.js
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/database"; 
+const firebaseConfig = {
+  
+  apiKey: "AIzaSyBzzaSTr42firr3fx5YcdXhDzB4iBLVBOM",
+  authDomain: "contact-database-9c47b.firebaseapp.com",
+  databaseURL: "https://contact-database-9c47b-default-rtdb.firebaseio.com",
+  projectId: "contact-database-9c47b",
+  storageBucket: "contact-database-9c47b.appspot.com",
+  messagingSenderId: "118822537955",
+  appId: "1:118822537955:web:3ef795baeadbe37b7293d6",
+  measurementId: "G-0WY0798WFC"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const auth = firebaseApp.auth();
+export const firestore = firebaseApp.firestore();
+export const database = firebaseApp.database(); 
