dfcc04ae238e486a107939202b25d109f765e321
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-02-04 12:52:06 +0600

    Update Header.svelte

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -10,15 +10,15 @@
       alt="Flowbite Logo"
     />
     <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-      Flowbite
+      Webui
     </span>
   </NavBrand>
   <NavHamburger on:click={toggle} />
   <NavUl {hidden}>
     <NavLi href="/" active={true}>Home</NavLi>
     <NavLi href="/about">About</NavLi>
-    <NavLi href="/services">Services</NavLi>
-    <NavLi href="/pricing">Pricing</NavLi>
+    <NavLi href="/login">Login</NavLi>
+    <NavLi href="/blog">Blog</NavLi>
     <NavLi href="/contact">Contact</NavLi>
   </NavUl>
 </Navbar>
