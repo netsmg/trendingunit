c57d766ecaa020d3acf7be0a5cee7c146631229b
NetSMG <150570330+netsmg@users.noreply.github.com>
2024-02-06 22:58:47 +0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,174 +1,53 @@
 <script>
-  import { onDestroy, onMount } from "svelte";
-  import PostList from "../lib/Components/PostList.svelte";
-  import { common } from "../stores/postStore";
-  import PostSkeleton from "../lib/Components/PostSkeleton.svelte";
-  
-import Search from "../lib/Components/Search.svelte";
-  
-  import SectionHead from "../lib/Components/SectionHead.svelte";
-  import {
-    collection,
-    query,
-    orderBy,
-    startAt,
-    endAt,
-    startAfter,
-    endBefore,
-    limit,
-    onSnapshot,
-    updateDoc,
-    doc,
-    increment,
-
-    getDocs
-
-  } from "firebase/firestore";
-  import { fstore } from "./firebase";
-
-
+  import { collection, onSnapshot } from 'firebase/firestore';
+  import { getFirestore } from 'firebase/firestore';
 
   let posts = [];
-  let lastVisible;
-  let lim = 20;
-  let postCount = 0;
+  const db = getFirestore();
+  const query = collection(db, 'products');
 
-  const makeData = (docS) => {
-    docS.forEach(doc=>{
-      posts.push({
-          ...doc.data(),
-          mins: $common.getReadTime(doc.data().content),
-          createdAt: $common.getDate(doc.data().createdAt),
-          id: doc.id
-        });
-    })
-
-      posts = posts;
-}
-
-  const  nextData = async(lastVisible, lim) => {
-      posts = [];
-    const q = query(
-      collection(fstore, "posts"),
-      orderBy("createdAt", "desc"),
-      startAfter(lastVisible),
-      limit(lim)
-    );
-
-    postCount++;
-
-    const docS = await getDocs(q);
-    lastVisible = docS.docs[docS.docs.length-1];
-    makeData(docS);
-
-      setTimeout(()=>{
-        MathJax.typeset();
-      }, 500);
-  }
-
-  const  prevData = async(lastVisible, lim) => {
-      posts = [];
-    const q = query(
-      collection(fstore, "posts"),
-      orderBy("createdAt", "desc"),
-      endAt(lastVisible),
-      limit(lim)
-    );
-
-    postCount--;
-
-    const docS = await getDocs(q);
-    lastVisible = docS.docs[docS.docs.length-1];
-    makeData(docS);
-
-      setTimeout(()=>{
-        MathJax.typeset();
-      }, 500);
-  }
-
-
-
-  onMount(async ()=>{
-    document.title = "Webui | Home";
-    document.description = "An authentic blog to share your code";
-    posts = [];
-    const q = query(
-      collection(fstore, "posts"),
-      orderBy("createdAt", "desc"),
-      limit(lim)
-    );
-
-    const docS = await getDocs(q);
-    lastVisible = docS.docs[docS.docs.length-1];
-    makeData(docS);
-
-      setTimeout(()=>{
-        MathJax.typeset();
-      }, 500);
+  onSnapshot(query, (snapshot) => {
+    posts = snapshot.docs.map((doc) => doc.data());
   });
-
-  const readHandle = async(id, read) => {
-    const postRef = doc(fstore, "posts", id);
-    try{
-      const res = await updateDoc(postRef, {
-        read: increment(1)
-      })
-      // console.log(res);
-    } catch(err){
-      console.log(err);
-    }
-  }
-
-  const next = () => {
-      if(posts.length>=10){
-        nextData(lastVisible, lim);
-      }
-  }
-
-  const prev = () => {
-      prevData(lastVisible, lim);
-  }
- 
 </script>
+<link rel="stylesheet" href="./css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="./css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="./css/owl.carousel.min.css">
+	<link rel="stylesheet" href="./css/magnific-popup.css">
+	<link rel="stylesheet" href="./css/nouislider.min.css">
+	<link rel="stylesheet" href="./css/jquery.mCustomScrollbar.min.css">
+	<link rel="stylesheet" href="./css/paymentfont.min.css">
+	<link rel="stylesheet" href="./css/main (1).css">
+<section class="section main container">
 
+<div>
+  {#each posts as post}
+    <div class="col-12 col-md-12 col-lg-6">
+      <div class="post post--big">
+        <a href="{post.link}" class="post__img">
+          <img src="{post.image}" alt="{post.title}">
+        </a>
 
-<Search/>
-
-<div class="absolute z-[-1] lg:left-[0] lg:top-[10px] top-[50px] md:top-10 md:left-[250px]">
-  
-</div>
-
-
-{#if posts.length > 0}
-<SectionHead title="Most Recent"/>
-  {#each posts as post, index}
-    <a href="/blog/{post.id}">
-      <PostList
-      on:click={()=> readHandle(post.id, post.read)}
-        read={post.read}
-        commentCount = {post.comment}
-        author={post.author}
-        title={post.title}
-        express={post.express}
-        tags={post.tags}
-        createdAt={post.createdAt}
-        mins={post.mins}
-        avatar="https://codebuckblog.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FLogo.e0b9e0a0.png&w=128&q=75"
-      />
-    </a>
+        <div class="post__content">
+          <a href="{post.link}" class="post__category">{post.tag}</a>
+          <h3 class="post__title"><a href="{post.link}">{post.title}</a></h3>
+          <div class="post__meta">
+            <span class="post__date">
+              <svg xmlns='http://www.w3.org/2000/svg' width='512' height='512' viewBox='0 0 512 512'>
+                <path d='M256,64C150,64,64,150,64,256s86,192,192,192,192-86,192-192S362,64,256,64Z' style='fill:none;stroke-miterlimit:10;stroke-width:32px'/>
+                <polyline points='256 128 256 272 352 272' style='fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/>
+              </svg> {post.date}
+            </span>
+            <span class="post__comments">
+              <svg xmlns='http://www.w3.org/2000/svg' width='512' height='512' viewBox='0 0 512 512'>
+                <path d='M431,320.6c-1-3.6,1.2-8.6,3.3-12.2a33.68,33.68,0,0,1,2.1-3.1A162,162,0,0,0,464,215c.3-92.2-77.5-167-173.7-167C206.4,48,136.4,105.1,120,180.9a160.7,160.7,0,0,0-3.7,34.2c0,92.3,74.8,169.1,171,169.1,15.3,0,35.9-4.6,47.2-7.7s22.5-7.2,25.4-8.3a26.44,26.44,0,0,1,9.3-1.7,26,26,0,0,1,10.1,2L436,388.6a13.52,13.52,0,0,0,3.9,1,8,8,0,0,0,8-8,12.85,12.85,0,0,0-.5-2.7Z' style='fill:none;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px'/>
+                <path d='M66.46,232a146.23,146.23,0,0,0,6.39,152.67c2.31,3.49,3.61,6.19,3.21,8s-11.93,61.87-11.93,61.87a8,8,0,0,0,2.71,7.68A8.17,8.17,0,0,0,72,464a7.26,7.26,0,0,0,2.91-.6l56.21-22a15.7,15.7,0,0,1,12,.2c18.94,7.38,39.88,12,60.83,12A159.21,159.21,0,0,0,284,432.11' style='fill:none;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px'/>
+              </svg> {post.comments}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   {/each}
-  <div class="flex items-center justify-center gap-5">
-    {#if postCount!=0}
-    <button on:click={()=> prev()} class="btn variant-filled-primary">Previous</button>
-    {/if}
-
-    {#if posts.length>9}
-    <button on:click={()=> next()} class="btn variant-filled-primary">Next</button>
-    {/if}
-  </div>
-{:else}
-  <PostSkeleton />
-  <PostSkeleton />
-  <PostSkeleton />
-  <PostSkeleton />
-{/if}
+</div>
+</section>
